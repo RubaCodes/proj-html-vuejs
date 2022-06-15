341ddf031ad03680f0636e7a6b8cde34b1aa8892
rubacodes <rubacodes@gmail.com>
2022-06-15 12:47:37 +0200

    completato hero manca play

--- src/components/BaseHero.vue
+++ src/components/BaseHero.vue
@@ -1,20 +1,31 @@
 <template>
-  <section class="bg-slate-900 bg-hero-pattern bg-no-repeat bg-right" id="hero">
-    <div class="container mx-auto text-white uppercase py-96" id="hero-title">
+  <section
+    class="bg-slate-900 bg-hero-pattern bg-no-repeat bg-right bg-contain"
+    id="hero"
+  >
+    <div
+      class="container mx-auto text-white uppercase pt-80 pb-72"
+      id="hero-title"
+    >
       <div class="flex gap-4 items-baseline">
         <hr class="w-24 hr" />
         <h3>always the best way you need it</h3>
       </div>
-      <h1 class="text-6xl w-96">the best businnes consulting</h1>
-      <!-- componetne successivo button -->
-      <button class="bg-white text-black mx-4">test</button>
-      <button class="bg-white text-black">Play button</button>
+      <h1 class="text-9xl w-2/5">the best businnes consulting</h1>
+      <BaseButton
+        class="mt-8"
+        :color="'bg-green-400'"
+        :innerText="'read more'"
+      />
+      <button class="ml-8">play</button>
     </div>
   </section>
 </template>
 
 <script>
+import BaseButton from './blocks/BaseButton.vue';
 export default {
+  components: { BaseButton },
   name: 'BaseHero',
 };
 </script>
---
+++ src/components/blocks/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="py-4 px-8 uppercase text-black" :class="color">
+    {{ innerText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    innerText: String,
+    color: String,
+  },
+};
+</script>
