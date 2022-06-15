e5c6125ceceba7477101503b23c605b13155b8f8
rubacodes <rubacodes@gmail.com>
2022-06-15 16:58:25 +0200

    ri-corretto spacing section strategy

--- src/components/BaseStrategy.vue
+++ src/components/BaseStrategy.vue
@@ -1,15 +1,15 @@
 <template>
   <section
-    class="bg-slate-900 bg-strategy-pattern bg-no-repeat bg-left"
+    class="bg-slate-900 bg-strategy-pattern bg-no-repeat bg-left bg-contain"
     id="strategy"
   >
-    <div class="container mx-auto text-white py-48" id="hero-title">
+    <div class="container mx-auto text-white py-56" id="hero-title">
       <div class="w-1/3 ml-auto">
         <div class="flex gap-4 items-baseline pb-6">
           <hr class="w-20 border-green-400 border-2" />
           <h3 class="text-xl uppercase">about us</h3>
         </div>
-        <h2 class="text-5xl uppercase">we make your strategy work</h2>
+        <h2 class="text-5xl uppercase font-bold">we make your strategy work</h2>
         <p class="pt-6">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
           repellendus esse, in optio beatae veritatis, qui ipsum facilis eius
@@ -22,11 +22,11 @@
       </div>
     </div>
     <div class="w-[95%] mr-auto bg-green-400 py-14 relative">
-      <div class="container w-3/5 mx-auto flex gap-80">
-        <h3 class="text-2xl uppercase w-3/12">
+      <div class="container w-3/5 mx-auto flex gap-80 items-center">
+        <h3 class="text-2xl uppercase w-3/12 font-bold">
           we help to deliver the business case
         </h3>
-        <p class="w-2/5">
+        <p class="w-2/5 text-sm">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
           consectetur aut officiis, iste tempore inventore blanditiis asperiores
           ex porro veniam facilis praesentium doloribus reiciendis deserunt
