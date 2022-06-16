0a66059bb71ecc2eca5f461438664a14ac762956
rubacodes <rubacodes@gmail.com>
2022-06-16 10:57:22 +0200

    prima implementazione hover  reviews

--- src/components/blocks/ReviewCard.vue
+++ src/components/blocks/ReviewCard.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="card px-20 py-16">
+  <div
+    @mouseenter="hovered = !hovered"
+    @mouseleave="hovered = !hovered"
+    class="card px-20 py-16"
+    :class="{ 'bg-[url(https://via.placeholder.com/501x436)]': hovered }"
+  >
     <p class="text-slate-500">{{ review.paragraph }}</p>
     <h4 class="font-bold pt-8">{{ review.name }}</h4>
     <h6 class="text-slate-500">{{ review.company }}</h6>
@@ -11,6 +16,11 @@ export default {
   name: 'ReviewCard',
   props: {
     review: Object,
+  },
+  data() {
+    return {
+      hovered: false,
+    };
   },
 };
 </script>
