56ec90f8e1254bdf244693168440608c3c91c282
rubacodes <rubacodes@gmail.com>
2022-06-16 13:34:53 +0200

    sostituito font con Inter di Google

--- src/components/BaseGetStarted.vue
+++ src/components/BaseGetStarted.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-get-started-banner bg-cover bg-no-repeat" id="get-started">
     <div
-      class="container mx-auto flex flex-col justify-center items-center text-white"
+      class="container mx-auto flex flex-col justify-center items-center text-white text-center"
     >
       <h2 class="text-6xl font-bold py-2 uppercase">let's talk about work</h2>
       <p class="pb-8 text-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'Sans-serif'],
+      },
       backgroundImage: {
         'hero-pattern':
           "url('/src/assets/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg')",
