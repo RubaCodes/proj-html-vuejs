2cc5b40ea97e6135c4715c1524f7b85ed3cefae6
rubacodes <rubacodes@gmail.com>
2022-06-16 15:55:41 +0200

    fix colore text hover button

--- src/components/blocks/BaseButton.vue
+++ src/components/blocks/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="uppercase hover:bg-primary-red hover: text-white hover:transition-colors px-8 py-4"
+    class="uppercase hover:bg-primary-red hover:text-white hover:transition-colors px-8 py-4"
     :class="color"
   >
     {{ innerText }}
