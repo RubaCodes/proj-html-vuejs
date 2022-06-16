6d254529e7e9e6581f6a77cbbfd9281f096b36b5
rubacodes <rubacodes@gmail.com>
2022-06-16 12:01:31 +0200

    impelemtato stato active navbar header

--- src/components/blocks/BaseNavLink.vue
+++ src/components/blocks/BaseNavLink.vue
@@ -1,5 +1,14 @@
 <template>
-  <a class="block py-8 uppercase text-lg" href="/">{{ linkName }}</a>
+  <a
+    @click="active = !active"
+    class="block py-8 uppercase text-lg border-b-4 border-t-4 border-b-transparent"
+    :class="{
+      ' border-t-4 border-white': active,
+      'border-t-transparent': !active,
+    }"
+    href="#"
+    >{{ linkName }}</a
+  >
 </template>
 
 <script>
@@ -7,6 +16,11 @@ export default {
   name: 'BaseNavLink',
   props: {
     linkName: String,
+  },
+  data() {
+    return {
+      active: false,
+    };
   },
 };
 </script>
