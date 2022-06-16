091f491f4d0b8e5dde10edc0c0e126142f13b2a9
rubacodes <rubacodes@gmail.com>
2022-06-16 11:33:58 +0200

    implelementato hover sezoine review

--- src/components/blocks/ReviewCard.vue
+++ src/components/blocks/ReviewCard.vue
@@ -11,9 +11,15 @@
       alt=""
     />
     <div class="absolute top-0 bottom-0 right-0 left-0 px-20 py-16">
-      <p class="text-slate-500">{{ review.paragraph }}</p>
-      <h4 class="font-bold pt-8">{{ review.name }}</h4>
-      <h6 class="text-slate-500">{{ review.company }}</h6>
+      <p :class="{ 'text-slate-300': hovered }" class="text-slate-500">
+        {{ review.paragraph }}
+      </p>
+      <h4 :class="{ 'text-white': hovered }" class="font-bold text-3xl pt-8">
+        {{ review.name }}
+      </h4>
+      <h6 :class="{ 'text-slate-300': hovered }" class="text-slate-500 text-xl">
+        {{ review.company }}
+      </h6>
     </div>
   </div>
 </template>
