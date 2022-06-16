6f73f3be02db5f88313113c3aefd840e8266b4e9
rubacodes <rubacodes@gmail.com>
2022-06-16 12:08:45 +0200

    prima impl hover buttons

--- src/components/blocks/BaseButton.vue
+++ src/components/blocks/BaseButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="py-4 px-8 uppercase" :class="color">
+  <button
+    class="py-4 px-8 uppercase hover:bg-red-500 hover: text-white hover:transition-colors"
+    :class="color"
+  >
     {{ innerText }}
   </button>
 </template>
